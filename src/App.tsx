fbb2dcfc34393b54061c78cf6aac0e59c3d4b262
prathameshpimpalkar <prathameshpimpalkar2002@gmail.com>
2025-03-17 22:24:19 +0530

    Refactor App component to include Navbar and Sidebar, and set up routing with BrowserRouter and Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import { Navbar } from "./components/Navbar";
+import { Sidebar } from "./components/Sidebar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
-  return <div>hi there</div>;
+  return (
+    <div>
+      <div className="flex">
+        <Sidebar />
+        <Navbar />
+      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" />
+          <Route path="/compare" />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;

03c3dea0408265b98f4d30fb1f6504e316e18c43
prathameshpimpalkar <prathameshpimpalkar2002@gmail.com>
2025-03-18 07:59:41 +0530

    Enhance Navbar and Sidebar components with fixed positioning and navigation functionality. Updated Sidebar buttons to handle routing and reflect active states based on the current path.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { Moon, User } from "lucide-react";
 
 export const Navbar = () => {
   return (
-    <div className="w-full">
+    <div className="w-full fixed md:relative bg-white">
       <div className="border-b p-3">
         <div className="flex max-w-7xl mx-auto justify-end gap-4">
           <Moon className="border rounded-full p-2 w-10 h-10" />
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,12 +5,19 @@ import {
   Package,
 } from "lucide-react";
 import { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export const Sidebar = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(true);
+  const navigate = useNavigate();
+  const location = useLocation();
 
   const handleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  const handleNavigation = (path: string) => {
+    navigate(path);
   };
 
   return (
@@ -34,13 +41,27 @@ export const Sidebar = () => {
               </button>
             </div>
             <div className="flex md:flex-col flex-row justify-center text-center items-center gap-4 mx-3 mt-4">
-              <button className="border p-2 rounded-xl w-full">
+              <button
+                onClick={() => handleNavigation("/")}
+                className={`p-2 rounded-lg w-full ${
+                  location.pathname === "/"
+                    ? "bg-slate-500 text-white"
+                    : "hover:bg-slate-500/50 cursor-pointer"
+                }`}
+              >
                 <span className="flex items-center gap-2 whitespace-nowrap">
                   <Package className="w-5 h-5" />
                   Product Details
                 </span>
               </button>
-              <button className="border p-2 rounded-xl w-full">
+              <button
+                onClick={() => handleNavigation("/compare")}
+                className={`p-2 rounded-lg w-full ${
+                  location.pathname === "/compare"
+                    ? "bg-slate-500 text-white"
+                    : "hover:bg-slate-500/50 cursor-pointer"
+                }`}
+              >
                 <span className="flex items-center gap-2 whitespace-nowrap">
                   <ArrowLeftRight className="w-5 h-5" />
                   Compare Products
@@ -56,26 +77,54 @@ export const Sidebar = () => {
             >
               <ChevronRight className="w-4 h-4" />
             </button>
-            <button>
+            <button
+              onClick={() => handleNavigation("/")}
+              className={`p-2 rounded-xl ${
+                location.pathname === "/"
+                  ? "bg-slate-500 text-white"
+                  : "hover:bg-slate-500/50 cursor-pointer"
+              }`}
+            >
               <Package className="w-5 h-5" />
             </button>
-            <button>
+            <button
+              onClick={() => handleNavigation("/compare")}
+              className={`p-2 rounded-xl ${
+                location.pathname === "/compare"
+                  ? "bg-slate-500 text-white "
+                  : "hover:bg-slate-500/50 cursor-pointer"
+              }`}
+            >
               <ArrowLeftRight className="w-5 h-5" />
             </button>
           </div>
         )}
       </div>
-      <div className="h-[60px] flex fixed bottom-0 border-t md:hidden w-full p-2 items-center gap-2">
-        <button className="border p-2 rounded-xl w-full">
+      <div className="h-[60px] flex fixed bottom-0 border-t md:hidden w-full p-2 items-center gap-2 bg-white">
+        <button
+          onClick={() => handleNavigation("/")}
+          className={`p-2 rounded-lg w-full ${
+            location.pathname === "/"
+              ? "bg-slate-500 text-white"
+              : "hover:bg-slate-500/50 cursor-pointer"
+          }`}
+        >
           <span className="flex items-center gap-2 whitespace-nowrap">
             <Package className="w-5 h-5" />
-            <h1>Product Details</h1>
+            Product Details
           </span>
         </button>
-        <button className="border p-2 rounded-xl w-full">
+        <button
+          onClick={() => handleNavigation("/compare")}
+          className={`p-2 rounded-md w-full ${
+            location.pathname === "/compare"
+              ? "bg-slate-500 text-white"
+              : "hover:bg-slate-500/50 cursor-pointer"
+          }`}
+        >
           <span className="flex items-center gap-2 whitespace-nowrap">
             <ArrowLeftRight className="w-5 h-5" />
-            <h1>Compare Products</h1>
+            Compare Products
           </span>
         </button>
       </div>

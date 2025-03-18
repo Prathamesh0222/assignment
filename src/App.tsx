ec54b3f9b84b8b612eb2f750daec9b12b779ec27
prathameshpimpalkar <prathameshpimpalkar2002@gmail.com>
2025-03-18 07:59:50 +0530

    Refactor App component to integrate Product component into routing structure, enhancing layout with responsive design and improved organization of Navbar and Sidebar.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
 import { Navbar } from "./components/Navbar";
+import { Product } from "./components/Product";
 import { Sidebar } from "./components/Sidebar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <div className="flex">
-        <Sidebar />
-        <Navbar />
-      </div>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" />
-          <Route path="/compare" />
-        </Routes>
+        <div className="flex">
+          <Sidebar />
+          <div className="flex-1">
+            <Navbar />
+            <div className="md:pt-0 pt-16 pb-16">
+              <Routes>
+                <Route path="/" element={<Product />} />
+                <Route path="/compare" />
+              </Routes>
+            </div>
+          </div>
+        </div>
       </BrowserRouter>
     </div>
   );
